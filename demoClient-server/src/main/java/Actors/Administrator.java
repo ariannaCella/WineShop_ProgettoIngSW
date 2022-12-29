9853f5cf26b87c7ccf702295ce339a8346ef5951
Elena Azzi <114754977+azzielena@users.noreply.github.com>
2022-12-29 09:54:36 +0100

    Delete demoClient-server directory
    mi serve per sistemare github

--- demoClient-server/src/main/java/Actors/Administrator.java
+++
@@ -1,8 +0,0 @@
-package Actors;
-
-public class Administrator extends User{
-	public Administrator(String n, String s, String fc, String e, int p,String a, int adm, String user, String passw) {
-		super(n,s,fc,e,p,a,0,user,passw);
-	}
-
-}
--- demoClient-server/src/main/java/com/example/democlientserver/OrderInvoice.java
+++
@@ -1,4 +0,0 @@
-package com.example.democlientserver;
-
-public class OrderInvoice {
-}
